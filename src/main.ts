cad76a6680aa8f7c4fa24e5f2d2ef56944e9dca1
SkadiD <50161715+skadiD@users.noreply.github.com>
2023-09-06 18:39:21 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { persist } from "pinia-persists";
 import { router } from "./router/router";
-import { fpjsPlugin } from '@fingerprintjs/fingerprintjs-pro-vue-v3';
 import VueClickAway from "vue3-click-away";
 import service from "./plugins/axios";
 import App from "./App.vue";
@@ -20,12 +19,6 @@ pinia.use(
   })
 );
 app.use(VueClickAway);
-app.use(fpjsPlugin, {
-  loadOptions: {
-    apiKey: "Q8Rj2cKiLVb9VVjSTZcG",
-    endpoint: "https://closure-fingerprint.arknights.host"
-  },
-})
 app.component("layout", BaseLayout);
 app.config.globalProperties.$axios = service;
 app.use(router).use(pinia).mount("#app");
